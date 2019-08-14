dcc30a4c98b6f378dc7cef6c9f366e40dc4da142
georgen2 <negraiageorge@yahoo.ro>
2019-08-14 11:19:07 +0300

    Some refactoring

--- Login.php
+++ Login.php
@@ -2,8 +2,10 @@
 include "Connection.php";
 global $connection;
 connect();
-$user = $_POST['user'];
-$pswd = $_POST['pswd'];
-$query = "INSERT INTO students (username,password) VALUES ('$user', '$pswd')";
-$result = mysqli_query($connection, $query);
+if (isset($_POST['sent'])) {
+    $user = $_POST['user'];
+    $pswd = $_POST['pswd'];
+    $query = "INSERT INTO students (username,password) VALUES ('$user', '$pswd')";
+    $result = mysqli_query($connection, $query);
+}
 ?>
